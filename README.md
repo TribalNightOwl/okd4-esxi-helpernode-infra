4696ac722ef4a6fdab36d6f12ca6a02453d3aa3a
Hugo Gayosso <tribrhy@gmail.com>
2020-08-15 20:35:40 -0400

    Major changes, easier configuration

--- README.md
+++ README.md
@@ -7,4 +7,17 @@ Set up ESXi infra for ocp4-helpernode
 # Before executing the deployment
 
 Load the necessary ssh keys into an ssh-agent
+
 Create a .env file based on the env.example from the repo
+
+Configure playbooks/vars.yaml to fit your environment
+
+# To install the helper node
+
+`helpernode.sh deploy`
+
+and follow the prompts
+
+# To remove the helper node
+
+`helpernode.sh destroy`
---
+++ helpernode.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+COMMAND=$1
+
+case "$COMMAND" in
+        deploy ) source .env
+                export PUBSSHKEY
+                ansible-playbook playbooks/configure-project.yaml
+                ./deploy-helpernode.sh ;;
+
+        destroy ) source config.sh
+                docker_run /files/terraform "terraform destroy -auto-approve"
+                rm -f deploy-helpernode.sh
+                rm -f files/ks.cfg
+                rm -f files/vars.yaml
+                rm -f kubeadmin-password
+                rm -f kubeconfig
+                rm -f config.sh
+                rm -f terraform/main.tf
+                rm -f terraform/vars.tf;;    
+        * ) cat <<EOT
+Usage:
+        helpernode.sh deploy
+        helpernode.sh destroy
+EOT
+        exit 0;;
+esac
