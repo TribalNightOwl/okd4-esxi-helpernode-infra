f645b76dd46b76a93593554d609ad1c4fdfeb115
Hugo Gayosso <tribrhy@gmail.com>
2020-09-13 11:50:46 -0400

    Detect if pull secret and/or .env are not present

--- helpernode.sh
+++ helpernode.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 COMMAND=$1
 
+function initialize_project {
+        if [[ ! -e files/pull-secret.json ]] ; then
+                printf "\nObtain your RedHat pull secret from: https://cloud.redhat.com/openshift/install/metal\n"
+                printf "and create the 'files/pull-secret.json' file\n"
+                read -p "Press <ENTER> when done" CONTINUE
+        fi
+
+        if [[ ! -e .env ]] ; then
+                read -s -p "Enter the password for your ESXi server: " TF_VAR_esxi_password
+                printf "\n"
+                read -s -p "Enter the public SSH key used to ssh into the hosts: " PUBSSHKEY
+
+                echo TF_VAR_esxi_password=${TF_VAR_esxi_password} >> .env
+                echo PUBSSHKEY=\'${PUBSSHKEY}\' >> .env
+        fi
+}       
+
 case "$COMMAND" in
-        deploy ) source .env
+        deploy ) initialize_project
+                source .env
                 export PUBSSHKEY
                 ansible-playbook playbooks/configure-project.yaml
                 ./deploy-helpernode.sh ;;
